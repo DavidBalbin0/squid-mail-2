e253876f4832a81a349c7cd278e37c679de417a1
balbino <debarbino@gmail.com>
2024-08-29 22:13:06 -0300

    fix: drawer

--- app/src/main/java/com/david/squid_mail/components/Drawer.kt
+++ app/src/main/java/com/david/squid_mail/components/Drawer.kt
@@ -1,14 +1,19 @@
 package com.david.squid_mail.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 @Composable
 fun DrawerMenu() {
-    Column {
+    Column (
+        modifier = Modifier.fillMaxWidth().wrapContentHeight()
+    ){
         Text("Rascunhos", modifier = Modifier.padding(16.dp))
         Text("Enviados", modifier = Modifier.padding(16.dp))
         Text("Deletados", modifier = Modifier.padding(16.dp))
