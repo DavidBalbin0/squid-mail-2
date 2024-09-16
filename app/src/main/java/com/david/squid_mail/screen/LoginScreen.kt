c667ba3927cfed3b607f209ddc8ed230087ec862
Fernando Ortriwano <fernandostampfl@gmail.com>
2024-09-15 23:24:01 -0300

    New commit

--- app/src/main/java/com/david/squid_mail/screen/LoginScreen.kt
+++ app/src/main/java/com/david/squid_mail/screen/LoginScreen.kt
@@ -1,12 +1,16 @@
 package com.david.squid_mail.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
@@ -21,13 +25,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.david.squid_mail.R
@@ -43,15 +53,36 @@ fun LoginScreen(viewModel: LoginViewModel, navController: NavController){
     Column (
         Modifier
             .fillMaxSize()
+            .background(Color(0xFF278AB0))
             .padding(16.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        Text(
+            text = "Bem vindo de volta!",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.SansSerif,
+            color = MaterialTheme.colorScheme.onPrimary,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(80.dp))
+
         OutlinedTextField(
             value = email,
             onValueChange = { viewModel.onEmailChange(it) },
-            label = { Text("E-mail") },
-            modifier = Modifier.fillMaxWidth(),
+            label = { Text(
+                "E-mail",
+                color = Color.Gray,
+                fontSize = 14.sp,
+                fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Center
+                ) },
+            modifier = Modifier
+                .width(280.dp)
+                .height(50.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(Color.White),
             isError = emailError.isNotEmpty(),
             keyboardOptions = KeyboardOptions.Default.copy(
                 keyboardType = KeyboardType.Email,
@@ -75,7 +106,11 @@ fun LoginScreen(viewModel: LoginViewModel, navController: NavController){
             value = password,
             onValueChange = { viewModel.onPasswordChange(it) },
             label = { Text("Senha") },
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .width(280.dp)
+                .height(50.dp)
+                .clip(RoundedCornerShape(24.dp))
+                .background(Color.White),
             isError = passwordError.isNotEmpty(),
             visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
             trailingIcon = {
@@ -100,21 +135,20 @@ fun LoginScreen(viewModel: LoginViewModel, navController: NavController){
             )
         }
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(70.dp))
 
         Button(
             onClick = { viewModel.login() },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .width(280.dp)
+                .height(50.dp)
+                .clip(RoundedCornerShape(24.dp)),
         ) {
-            Text("Login")
+            Text(
+                text = "Entrar",
+                color = Color.Black,
+                fontSize = 16.sp,)
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(
-            text = "Não é registrado? Clique aqui.",
-//            color = MaterialTheme.colorScheme,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
 
         Spacer(modifier = Modifier.height(16.dp))
         Text(
--- app/src/main/java/com/david/squid_mail/screen/WelcomeScreen.kt
+++ app/src/main/java/com/david/squid_mail/screen/WelcomeScreen.kt
@@ -1,63 +1,88 @@
 package com.david.squid_mail.screen
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.coroutineScope
 
 @Composable
-
-fun WelcomeScreen(navController: NavController){
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Bem-vindo",
-            style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.padding(bottom = 56.dp)
-            )
-        Text(
-            text = "Faça login ou cadastre-se para continuar",
-            style = MaterialTheme.typography.titleMedium,
-            color = MaterialTheme.colorScheme.onBackground,
-            modifier = Modifier.padding(bottom = 250.dp)
-            )
-
-        Button(
-            onClick = { navController.navigate("login") },
-            modifier = Modifier.fillMaxWidth().padding(top = 150.dp)
+fun WelcomeScreen(navController: NavController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF278AB0)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Entrar")
-        }
-        Button(
-            onClick = { navController.navigate("register")  },
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Registrar-se")
+            Text(
+                text = "Bem-vindo ao SquidMail",
+                style = MaterialTheme.typography.titleLarge,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = "Conte com a gente para facilitar sua comunicação, sempre com segurança e praticidade",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+            Text(
+                text = "Faça login ou cadastre-se para continuar",
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+            Button(
+                onClick = { /* Simular navegação / },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            ) {
+                Text("Entrar")
+            }
+            Button(
+                onClick = { / Simular navegação */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Já tenho conta")
+            }
+            Button(
+                onClick = { /* Simular navegação / },
+                modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            ) {
+                Text("Entrar")
+            }
+            Button(
+                onClick = { / Simular navegação */ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Quero me cadastrar")
+            }
         }
     }
 }
 
-
-
 @Preview(showBackground = true)
 @Composable
-fun WelcomeScreenPreview(){
-    var navController = rememberNavController()
-    WelcomeScreen(navController)
+fun WelcomeScreenPreview() {
+    WelcomeScreen(navController = rememberNavController())
 }
-
