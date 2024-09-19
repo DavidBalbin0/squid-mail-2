34b873a9216d5b0a8a8e529db7af39196760442b
Lucassuyama <lucasuyama@hotmail.com>
2024-09-18 23:06:11 -0300

    Cadastrov5

--- app/src/main/java/com/david/squid_mail/screen/EmailScreen.kt
+++ app/src/main/java/com/david/squid_mail/screen/EmailScreen.kt
@@ -1,10 +1,15 @@
 package com.david.squid_mail.screen
 
 import android.util.Log
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -19,8 +24,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.david.squid_mail.components.DefaultTopBar
@@ -101,7 +110,13 @@ fun EmailScreen(
                         navController.navigate("email-composition")
                     }
                 ) {
-                    Icon(Icons.Filled.Add, contentDescription = "Compose email")
+                    Icon(Icons.Filled.Create, contentDescription = "Compose email",
+                        modifier = Modifier
+                            .size(100.dp)
+
+                            )
+
+
                 }
             }
 
